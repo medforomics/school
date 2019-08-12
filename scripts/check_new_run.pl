2321b883b568c20a9ef80c26e1e3dca7e55479ad
Brandi Cantarel <brandi.cantarel@utsouthwestern.edu>
2019-08-12 16:50:16 -0500

    typos scripts location

--- scripts/check_new_run.pl
+++ scripts/check_new_run.pl
@@ -4,9 +4,8 @@
 my $refdir = "/project/shared/bicf_workflow_ref/human/GRCh38";
 my @scriptpath = split(/\//,$0);
 pop @scriptpath;
+pop @scriptpath;
 my $execdir = join("/",@scriptpath);
-
-exit;
 
 my @samplesheets = `/bin/ls /project/PHG/PHG_BarTender/bioinformatics/sample_sheets/*.csv`;
 chomp(@samplesheets);
@@ -19,11 +18,11 @@ foreach my $ss (@samplesheets) {
   unless (-e "/project/PHG/PHG_Clinical/illumina/sample_sheets/$fname") {
       system("cp $ss /project/PHG/PHG_Clinical/illumina/sample_sheets/$fname");
       system("source /etc/profile.d/modules.sh");
-      system(qq{$execdir\/create_directories.sh -p $prjid -c $prodir});
-      system(qq{cp $execdir\/init_workflows.sh $procbase});
-      open OUT, ">run_$prjid.sh" or die $!;
+      system(qq{$execdir\/scripts/create_directories.sh -p $prjid -c $prodir});
+      system(qq{cp $execdir\/scripts/init_workflows.sh $procbase});
+      open OUT, ">$procbase\/run_$prjid.sh" or die $!;
       print OUT qq{#!/bin/bash\n$procbase\/init_workflows.sh -p $prjid -b $execdir -c $prodir -r $refdir -x\n};
       close OUT;
-      system("sbatch -p 32GB run_$prjid.sh");
+      system("sbatch -p 32GB $procbase\/run_$prjid.sh");
   }
 }
