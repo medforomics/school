f79c7fc1ea867379dfd02aeb6b6365ed5d9e3d79
Brandi Cantarel <brandi.cantarel@utsouthwestern.edu>
2020-03-12 15:29:03 -0500

    updates for hardcoded paths, svcalling integration in unify_case.sh

--- scripts/syncCase2Azure.sh
+++ scripts/syncCase2Azure.sh
@@ -3,6 +3,13 @@
 set -e
 caseID=$1
 nucliatoken=$2
+wrkdir=$3
+
+if [[ -z $wrkdir ]]
+then
+    wkdir= /archive/PHG/PHG_Clinical
+fi
+
 baseDir="`dirname \"$0\"`"
 
 module load samtools/gcc/1.8 azure/2.0.72 
@@ -29,76 +36,77 @@ then
 fi
 monyear="20${runid:0:4}"
 
-if [[ ! -d /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]} ]]
+if [[ ! -d $wkdir/casesTemp/${myarray[1]} ]]
 then
-    mkdir /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}
+    mkdir $wkdir/casesTemp/${myarray[1]}
 fi
-if [[ -d /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[3]} ]]
+if [[ -d $wkdir/cases/${myarray[1]}/${myarray[3]} ]]
 then
-    if [[ ! -d /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[3]} ]]
+    if [[ ! -d $wkdir/casesTemp/${myarray[1]}/${myarray[3]} ]]
     then
-	mkdir /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[3]}
+	mkdir $wkdir/casesTemp/${myarray[1]}/${myarray[3]}
     fi
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}*answer* /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[3]}
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[1]}.vcf.gz /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}
+    cp $wkdir/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}*answer* $wkdir/casesTemp/${myarray[1]}/${myarray[3]}
+    cp $wkdir/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.ballelefreq.txt $wkdir/casesTemp/${myarray[1]}/${myarray[3]}
+    cp $wkdir/cases/${myarray[1]}/${myarray[1]}.vcf.gz $wkdir/casesTemp/${myarray[1]}
 fi
-if [[ -f /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.consensus.bam ]]
+if [[ -f $wkdir/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.consensus.bam ]]
 then
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.consensus.bam /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[3]}/${myarray[3]}.bam
-    samtools index -@ 4 /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[3]}/${myarray[3]}.bam
-elif [[ -f /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.final.bam ]]
+    cp $wkdir/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.consensus.bam $wkdir/casesTemp/${myarray[1]}/${myarray[3]}/${myarray[3]}.bam
+    samtools index -@ 4 $wkdir/casesTemp/${myarray[1]}/${myarray[3]}/${myarray[3]}.bam
+elif [[ -f $wkdir/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.final.bam ]]
 then
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.final.bam /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[3]}/${myarray[3]}.bam
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.final.bai /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[3]}/${myarray[3]}.bam.bai
+    cp $wkdir/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.final.bam $wkdir/casesTemp/${myarray[1]}/${myarray[3]}/${myarray[3]}.bam
+    cp $wkdir/cases/${myarray[1]}/${myarray[3]}/${myarray[3]}.final.bai $wkdir/casesTemp/${myarray[1]}/${myarray[3]}/${myarray[3]}.bam.bai
 fi
-if [[ -d /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[5]} ]]
+if [[ -d $wkdir/cases/${myarray[1]}/${myarray[5]} ]]
 then
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[1]}.concordance.txt /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[1]}.utswpass.somatic.vcf* /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[1]}.TMB.csv /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}
-    if [[ -f /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[1]}.utswpass.somatic.vcf.gz ]]
+    cp $wkdir/cases/${myarray[1]}/${myarray[1]}.concordance.txt $wkdir/casesTemp/${myarray[1]}
+    cp $wkdir/cases/${myarray[1]}/${myarray[1]}.utswpass.somatic.vcf* $wkdir/casesTemp/${myarray[1]}
+    cp $wkdir/cases/${myarray[1]}/${myarray[1]}.TMB.csv $wkdir/casesTemp/${myarray[1]}
+    if [[ -f $wkdir/casesTemp/${myarray[1]}/${myarray[1]}.utswpass.somatic.vcf.gz ]]
     then
-	gunzip /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[1]}.utswpass.somatic.vcf.gz
+	gunzip $wkdir/casesTemp/${myarray[1]}/${myarray[1]}.utswpass.somatic.vcf.gz
     fi
-    if [[ ! -d /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[5]} ]]
+    if [[ ! -d $wkdir/casesTemp/${myarray[1]}/${myarray[5]} ]]
     then
-	mkdir /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[5]}
+	mkdir $wkdir/casesTemp/${myarray[1]}/${myarray[5]}
     fi
-    if [[ -f /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[5]}/${myarray[5]}.consensus.bam ]]
+    if [[ -f $wkdir/cases/${myarray[1]}/${myarray[5]}/${myarray[5]}.consensus.bam ]]
     then
-	cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[5]}/${myarray[5]}.consensus.bam /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[5]}/${myarray[5]}.bam
-	samtools index -@ 4 /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[5]}/${myarray[5]}.bam
+	cp $wkdir/cases/${myarray[1]}/${myarray[5]}/${myarray[5]}.consensus.bam $wkdir/casesTemp/${myarray[1]}/${myarray[5]}/${myarray[5]}.bam
+	samtools index -@ 4 $wkdir/casesTemp/${myarray[1]}/${myarray[5]}/${myarray[5]}.bam
     else
-	cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[5]}/${myarray[5]}.final.bam /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[5]}/${myarray[5]}.bam
-	samtools index -@ 4 /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[5]}/${myarray[5]}.bam
+	cp $wkdir/cases/${myarray[1]}/${myarray[5]}/${myarray[5]}.final.bam $wkdir/casesTemp/${myarray[1]}/${myarray[5]}/${myarray[5]}.bam
+	samtools index -@ 4 $wkdir/casesTemp/${myarray[1]}/${myarray[5]}/${myarray[5]}.bam
     fi	   
 fi
     
-if [[ -f /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.bam ]]
+if [[ -f $wkdir/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.bam ]]
 then
-    if [[ ! -d /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[7]} ]]
+    if [[ ! -d $wkdir/casesTemp/${myarray[1]}/${myarray[7]} ]]
     then
-	mkdir /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[7]}
+	mkdir $wkdir/casesTemp/${myarray[1]}/${myarray[7]}
     fi
-    if [[ -f /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.bamreadct.txt ]]
+    if [[ -f $wkdir/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.bamreadct.txt ]]
     then
-	gzip /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.bamreadct.txt
+	gzip $wkdir/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.bamreadct.txt
     fi
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.bam /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[7]}
-    samtools index -@ 4 /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[7]}/${myarray[7]}.bam
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.cts /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[7]}
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.fpkm.txt /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[7]}
-    cp /archive/PHG/PHG_Clinical/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.translocations.answer.txt /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]}/${myarray[7]}
+    cp $wkdir/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.bam $wkdir/casesTemp/${myarray[1]}/${myarray[7]}
+    samtools index -@ 4 $wkdir/casesTemp/${myarray[1]}/${myarray[7]}/${myarray[7]}.bam
+    cp $wkdir/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.cts $wkdir/casesTemp/${myarray[1]}/${myarray[7]}
+    cp $wkdir/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.fpkm.txt $wkdir/casesTemp/${myarray[1]}/${myarray[7]}
+    cp $wkdir/cases/${myarray[1]}/${myarray[7]}/${myarray[7]}.translocations.answer.txt $wkdir/casesTemp/${myarray[1]}/${myarray[7]}
 fi
 
-mv /archive/PHG/PHG_Clinical/cases/${myarray[1]} /archive/PHG/PHG_Clinical/toarchive/caseDirs/${myarray[1]}
-mv /archive/PHG/PHG_Clinical/casesTemp/${myarray[1]} /archive/PHG/PHG_Clinical/cases
-rsync -avz /archive/PHG/PHG_Clinical/cases/${myarray[1]} answerbe@198.215.54.71:/swnas/cases
+mv $wkdir/cases/${myarray[1]} $wkdir/toarchive/caseDirs/${myarray[1]}
+mv $wkdir/casesTemp/${myarray[1]} $wkdir/cases
+rsync -avz $wkdir/cases/${myarray[1]} answerbe@198.215.54.71:/swnas/cases
 
-if [[ ! -f  /archive/PHG/PHG_Clinical/toarchive/backups/${monyear}/${myarray[1]}.tar.gz ]]
+if [[ ! -f  $wkdir/toarchive/backups/${monyear}/${myarray[1]}.tar.gz ]]
 then
-    tar cf /archive/PHG/PHG_Clinical/toarchive/backups/${monyear}/${myarray[1]}.tar /archive/PHG/PHG_Clinical/toarchive/caseDirs/${myarray[1]}
-    gzip /archive/PHG/PHG_Clinical/toarchive/backups/${monyear}/${myarray[1]}.tar 
+    tar cf $wkdir/toarchive/backups/${monyear}/${myarray[1]}.tar $wkdir/toarchive/caseDirs/${myarray[1]}
+    gzip $wkdir/toarchive/backups/${monyear}/${myarray[1]}.tar 
 fi 
 
 isfalse=`az storage container exists -n archive${monyear} | grep false`
@@ -107,4 +115,4 @@ then
     az storage container create -n archive${monyear} --fail-on-exist
 fi
 
-az storage blob upload -c archive${monyear} -f /archive/PHG/PHG_Clinical/toarchive/backups/${monyear}/${myarray[1]}.tar.gz -n ${myarray[1]}.tar.gz
+az storage blob upload -c archive${monyear} -f $wkdir/toarchive/backups/${monyear}/${myarray[1]}.tar.gz -n ${myarray[1]}.tar.gz
