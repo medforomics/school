bd179966f543584ef7aa7e1363586a46c82e90a0
Brandi Cantarel <brandi.cantarel@utsouthwestern.edu>
2020-03-30 14:38:24 -0500

    add viral analysis

--- scripts/validation_json2txt.pl
+++ scripts/validation_json2txt.pl
@@ -11,18 +11,22 @@ $prefix = (split(/\./,$jsonfile))[0];
 my @vtypes = ('variants','cnvs','translocations');
 
 foreach my $vtype (@vtypes) {
-    my %csam;
-    open OUT, ">$prefix\.$vtype\.txt" or die $!;
-    foreach $tref (@{$jsonref->{$vtype}}) {
-	%hash = %{$tref};
-	next unless ($hash{selected});
-	if ($vtype eq 'variants') {
-	    print OUT join("\t",$prefix,$hash{'caseId'},$hash{chrom},$hash{pos},$hash{reference},$hash{alt},$hash{'tumorTotalDepth'},sprintf("%.2f",$hash{'tumorAltFrequency'}),
-			   $hash{'normalTotalDepth'},sprintf("%.2f",$hash{'normalAltFrequency'}),$hash{'geneName'},$hash{'notation'}),"\n";
-	}elsif ($vtype eq 'cnvs') {
-	    my @genes = @{$hash{genes}};
-	    foreach my $gid (@genes) {
-		print OUT join("\t",$prefix,$hash{'caseId'},$gid,$hash{'copyNumber'},$hash{'aberrationType'},$hash{cytoband},$hash{score}),"\n";
+  my %csam;
+  open OUT, ">$prefix\.$vtype\.txt" or die $!;
+  foreach $tref (@{$jsonref->{$vtype}}) {
+    %hash = %{$tref};
+    #next unless ($hash{selected});
+    if ($vtype eq 'variants') {
+      print OUT join("\t",$prefix,$hash{chrom},$hash{pos},$hash{reference},
+		     $hash{alt},$hash{'tumorTotalDepth'},
+		     sprintf("%.2f",$hash{'tumorAltFrequency'}),
+		     $hash{'normalTotalDepth'},
+		     sprintf("%.2f",$hash{'normalAltFrequency'}),
+		     $hash{'geneName'},$hash{'notation'}),"\n";
+    }elsif ($vtype eq 'cnvs') {
+      my @genes = @{$hash{genes}};
+      foreach my $gid (@genes) {
+	print OUT join("\t",$prefix,$hash{'caseId'},$gid,$hash{'copyNumber'},$hash{'aberrationType'},$hash{cytoband},$hash{score}),"\n";
 	    }
 	}else {
 	    print OUT join("\t",$prefix,$hash{'caseId'},$hash{"fusionName"},$hash{"rnaReads"}),"\n";
