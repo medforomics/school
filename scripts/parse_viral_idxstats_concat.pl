672f5e9cf2f1eb81642bcc965c889e187e549ccd
Brandi Cantarel <brandi.cantarel@utsouthwestern.edu>
2020-04-22 08:17:06 -0500

    msi pro; viral output to answer; mutsig code

--- scripts/parse_viral_idxstats_concat.pl
+++ scripts/parse_viral_idxstats_concat.pl
@@ -15,23 +15,15 @@ while (my $line = <GNAME>) {
 }
 
 open OUT, ">viral_results.txt" or die $!;
-print OUT join("\t","SampleID","VirusName","VirusAcc","VirusDescription","ViralReadCt"),"\n";
 
 my @idxstats = @ARGV;
 foreach $file (@idxstats) {
     open IN, "<$file" or die $!;
-    my ($sample,$filename) = split(/\//,$file);
-    my $header = <IN>;
-    chomp($header);
-    my @colnames = split(/\t/,$header);
+    my ($case,$sample,$filename) = split(/\//,$file);
     while (my $line = <IN>) {
 	chomp($line);
-	my @row = split(/\t/,$line);
-	my %hash;
-	foreach my $i (0..$#row) {
-	    $hash{$colnames[$i]} = $row[$i];
-	}
-	$hash{VirusAcc} = (split(/\./,$hash{VirusAcc}))[0];
-	print OUT join("\t",$sample,$genoname{$hash{VirusAcc}},$hash{VirusAcc},$hash{VirusName},$hash{Mapped}),"\n" if ($hash{Mapped} > 5);
+	my ($geno,$length,$mapped,$unmapped) = split(/\t/,$line);
+	$geno = (split(/\./,$geno))[0];
+	print OUT join("\t",$case,$sample,$genoname{$geno},$geno,$mapped),"\n" if ($mapped > 10);
     }
 }
